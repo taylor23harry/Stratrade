cc1c7665f3cd8e4f41e63b8a7bbbd617d2b1cea1
taylor23harry <taylor23harry@gmail.com>
2023-04-30 23:49:59 +0200

    Missed from inital commit

---
+++ src/main/java/com/taylor23harry/stratrade/StratradeApplication.java
@@ -0,0 +1,13 @@
+package com.taylor23harry.stratrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StratradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StratradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taylor23harry/stratrade/StratradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taylor23harry.stratrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StratradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
